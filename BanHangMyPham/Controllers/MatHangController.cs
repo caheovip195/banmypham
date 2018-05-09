0235b6541bbee72669e237c7da5b65130bd5e0de
Nguyen Cong <ntcong95.oplai@gmail.com>
2018-05-09 19:31:16 +0700

    first commit

---
+++ BanHangMyPham/Controllers/MatHangController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BanHangMyPham.Controllers
+{
+    public class MatHangController : ApiController
+    {
+        //Tra ve tat ca san pham
+        [HttpGet]
+        public List<MatHang> dsmathang()
+        {
+            QuanLyMyPhamDataContext context = new QuanLyMyPhamDataContext();
+            List<MatHang> ds = context.MatHangs.ToList();
+            foreach(MatHang mh in ds)
+                mh.TheLoai = null;
+            return ds;
+        }
+    }
+}
